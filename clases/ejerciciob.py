5d09c2f3a0feee67c17c361cae6bf7322503376c
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-28 20:26:16 +0200

    enunciado copiado

---
+++ clases/ejerciciob.py
@@ -0,0 +1,13 @@
+class yin: pass
+class yang: 
+    def __del__(self):
+        print("yang destruido")
+
+yin = yin()
+yang = yang()
+yin.yang = yang
+
+print(yang)
+print(yang is yin.yang)
+del(yang)
+print("?")
