1377eba5191a3b92ba7e052212fec60776256a9e
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-28 21:17:45 +0200

    clase nueva york completada

--- clases/ejercicioa.py
+++ clases/ejercicioa.py
@@ -1,4 +1,6 @@
 class newyork:
     def __del__(self):
         print("Se destruye New York")
-        
+    def __init__(self):
+        self.edificio = [edificio(name) for name in ["a", "b"]]
+        self.empleado = [empleado(name) for name in ["salim", "sñr Martin", "sra. Xing"]]
