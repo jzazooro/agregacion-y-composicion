abc3f6f9b32760b37f50bc653887e0ba5c9393a5
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-29 00:24:37 +0200

    correccion

--- clases/ejerciciob.py
+++ clases/ejerciciob.py
@@ -3,11 +3,3 @@ class yang:
     def __del__(self):
         print("yang destruido")
         print("?")
-
-yin = yin()
-yang = yang()
-yin.yang = yang
-
-print(yang)
-print(yang is yin.yang)
-del(yang)
