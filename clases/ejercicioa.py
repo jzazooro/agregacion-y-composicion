d3d0ef419ba192daaf696f67a2e9bdb18610c35d
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-28 21:19:38 +0200

    clase edificio

--- clases/ejercicioa.py
+++ clases/ejercicioa.py
@@ -4,3 +4,7 @@ class newyork:
     def __init__(self):
         self.edificio = [edificio(name) for name in ["a", "b"]]
         self.empleado = [empleado(name) for name in ["salim", "sñr Martin", "sra. Xing"]]
+
+class edificio: 
+    def __del_(self):
+        print("destruccion{}".format(self.name))
