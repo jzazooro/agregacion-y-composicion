1cd915cfbde8ed6e2db60b9b9e7d4a4d6d07edf5
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-30 04:00:18 +0200

    copiamos el codigo realizado en el trabajo de herencias

---
+++ clases/ejercicioc.py
@@ -0,0 +1,27 @@
+class pared: 
+    def __init__(self, horientacion):
+        self.horientacion=horientacion
+
+class ventana(pared):
+    def __init__(self, horientacion, superficie):
+        super().__init__(horientacion)
+        self.superficie=superficie
+        return self.superficie
+
+class casa(ventana):
+    def __init__(self, paredes, horientacion, superficie):
+        self.paredes=paredes
+    def superficieacristalada(self):
+        return(self.paredes.superficie)
+
+pared_norte = pared("NORTE") 
+pared_oeste = pared("OESTE")
+pared_sur = pared("SUR") 
+pared_este = pared("ESTE")
+ventana_norte = ventana(pared_norte, 0.5) 
+ventana_oeste = ventana(pared_oeste, 1) 
+ventana_sur = ventana(pared_sur, 2) 
+ventana_este = ventana(pared_este, 1)
+casa = casa([pared_norte, pared_oeste, pared_sur, pared_este]) 
+
+print(casa.superficieacristalada())
